f2cce9f8e0dc86563650b84ad823948d970e3036
zaza <sitiazzaleaannisa@gmail.com>
2021-03-15 19:56:57 +0700

    project euler 001 & 002

---
+++ src/org/zaza/hackerrank/euler/Euler001.java
@@ -0,0 +1,42 @@
+package org.zaza.hackerrank.euler;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Euler001 {
+	
+	public static long addMultiple(int number) {
+		long total = 0;
+		for (int i = 1; i < number; i++) {
+			if (i % 3 == 0 || i % 5 == 0) {
+				total += i;
+			}
+		}
+		return total;
+	}
+	
+	public static long addMultiple2(int number) {
+		long total = 0;
+//		3 + 6 + 9 + 12 + 15 + 18 = 3 (1 + 2 + 3 + 4 + 5 + 6) = 3 (6 *(6 + 1) / 2)
+		long multipleThree = (number-1) / 3;
+		long multipleFive = (number-1) / 5;
+		long multipleFifteen = (number-1) / 15;
+		long total3 = 3 * (multipleThree *(multipleThree + 1) / 2);
+		long total5 = 5 * (multipleFive *(multipleFive + 1) / 2);
+		long total15 = 15 * (multipleFifteen *(multipleFifteen + 1) / 2);
+		total = total3 + total5 - total15;
+		return total;
+	}
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for (int a0 = 0; a0 < t; a0++) {
+            int n = in.nextInt();
+            System.out.println(addMultiple2(n));
+        }
+    }
+}
---
+++ src/org/zaza/hackerrank/euler/Euler002.java
@@ -0,0 +1,35 @@
+package org.zaza.hackerrank.euler;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Euler002 {
+	
+	public static long generateFibonacci(long numberMax) {
+		long evenFibonacciTotal = 0;
+		long number1 = 0;
+		long number2 = 1;
+		while (number2 + number1 <= numberMax) {
+			long temp = number1 + number2;
+			number1 = number2;
+			number2 = temp;
+			if (temp % 2 == 0) {
+				evenFibonacciTotal += temp;
+			}
+		}
+		return evenFibonacciTotal;
+	}
+	
+	public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            long n = in.nextLong();
+    		System.out.println(generateFibonacci(n));
+        }
+    }
+
+}
